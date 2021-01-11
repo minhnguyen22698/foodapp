4a36e4474330a48ce27bb3afd941859b97aa1468
minhn <minhnguyen22698@gmail.com>
2021-01-11 16:51:06 +0700

    Đa ngôn ngữ

--- index.js
+++ index.js
@@ -5,6 +5,5 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import i18n from '../utils/i18n'
 
 AppRegistry.registerComponent(appName, () => App);
